b72567154782fdf99489284a2979f1800eb8a04a
Sjaak Luthart <mail@sjaakluthart.nl>
2016-02-20 16:42:07 +0100

    changed text

--- src/assets/react/info.jsx
+++ src/assets/react/info.jsx
@@ -7,7 +7,7 @@ var Info = React.createClass({
         <article>
           <h1>Artisoks</h1>
           <p>
-            Wij zijn drie illustratie studentes, <a href="http://mariekeluthart.nl" target="blank">Marieke Luthart</a>, Jolien van Brakel en Guusje Thelissen Samen drukken wij ons werk op sokjes.
+            Wij zijn drie illustratie studentes, <a href="http://mariekeluthart.nl" target="blank">Marieke Luthart</a>, Jolien van Brakel en Guusje Thelissen. Samen drukken wij ons werk op sokjes.
           </p>
           <h1>Contact</h1>
           <p><a href="mailto:mail@artisoks.nl">mail@artisoks.nl</a></p>
