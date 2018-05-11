eb6b139504a15d018d455abf3f0dc1977b4f3537
Sjaak Luthart <sjaak.luthart@anchor.chat>
2018-05-11 12:23:23 +0200

    Removed old project, WIP new one.

---
+++ scripts/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ -z "${BUILD_DIR}" ]; then
+  BUILD_DIR=./build
+fi
+
+cp -R assets build
+npm run postcss
+
+cat ${BUILD_DIR}/css/{meyer-reset.css,index.css} > bundle.css
+rm -rf ${BUILD_DIR}/css/
+
+cat templates/{header.html,home.html,footer.html} > ${BUILD_DIR}/index.html
+cat templates/{header.html,about.html,footer.html} > ${BUILD_DIR}/about.html
+
+mkdir build/assets/css
+
+CSS_HASH=$(shasum bundle.css | cut -b-10)
+mv bundle.css ${BUILD_DIR}/assets/css/bundle.${CSS_HASH}.css
+
+sed -i -e s@%%CSS_HASH%%@${CSS_HASH}@ ${BUILD_DIR}/*.html
